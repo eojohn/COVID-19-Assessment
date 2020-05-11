91f935924653aeeaffc4f4a64b3f3f42fa0f301c
Eon John <eonnigeljohn@gmail.com>
2020-05-11 17:54:07 -0400

    Auth Files

---
+++ Auth.js
@@ -0,0 +1,4 @@
+export const REACT_APP_API_KEY= 'AIzaSyBCtPkAmNcwPjaTtUcEwFu8pdm_Y1KtLC8'
+export const REACT_APP_AUTH_ID= 'covid-19-assessment-bccdf'
+export const REACT_APP_DB_ID= 'USERS'
+export const REACT_APP_ID= 'dfeaic6gq'
