126ac6321458e2515e57b5b78b17c426337cc1ea
Eon John <eonnigeljohn@gmail.com>
2020-05-11 17:48:07 -0400

    Delete Auth.js

--- Auth.js
+++
@@ -1,4 +0,0 @@
-export const REACT_APP_API_KEY= 'AIzaSyBCtPkAmNcwPjaTtUcEwFu8pdm_Y1KtLC8'
-export const REACT_APP_AUTH_ID= 'covid-19-assessment-bccdf'
-export const REACT_APP_DB_ID= 'USERS'
-export const REACT_APP_ID= 'dfeaic6gq'
